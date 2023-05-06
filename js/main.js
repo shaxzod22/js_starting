58d62689815833d0c97f7eeffc102d9459d8da62
shaxzod22 <shaxzod01901@gmail.com>
2023-05-06 20:23:13 +0500

    asking informations

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let ismi = prompt('Ismingiz nima!')
+let familiyasi = prompt('Familiyangiz nima!')
+let yoshi = prompt('Yoshingiz nechchida!')
+let manzili = prompt('Manzilingiz qayer!')
+
+let obj = {
+    ismingiz: ismi, 
+    familyangiz: familiyasi,
+    yoshingiz: yoshi,
+    turarJoyingiz: manzili
+}
+
+console.log(obj);
